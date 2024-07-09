8c20bc9ab21ec5d76a8e4c3101fa1f7e196b8ea2
JUNU <bigdwarf_@naver.com>
2024-07-09 21:43:53 +0900

    프로젝트 세팅

---
+++ src/main/java/spring/login/example/login_example/LoginExampleApplication.java
@@ -0,0 +1,13 @@
+package spring.login.example.login_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/login/example/login_example/LoginExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.login.example.login_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
